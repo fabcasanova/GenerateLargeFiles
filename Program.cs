e26414fc0bb08701fda46d36d29dac2dbd29f159
Fabian_C <54602317+fabcasanova@users.noreply.github.com>
2021-10-28 16:19:29 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generateLargeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
